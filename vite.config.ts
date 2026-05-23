d334028a46ff519c5fd5f4c823c11c985d3a8cfb
MubashirA4 <hamza.cse123@gmail.com>
2026-05-23 12:29:21 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     target: "esnext",
-    minify: "terser",
+    minify: "esbuild",
     cssMinify: true,
     rollupOptions: {
       output: {
